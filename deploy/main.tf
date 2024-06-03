9af21bf2ad75cfa0429280bde88fe2d61224a177
Erick Moore <erick.moore@gmail.com>
2024-06-03 16:55:31 -0500

    adding tags (#3)

--- deploy/main.tf
+++ deploy/main.tf
@@ -5,6 +5,9 @@ provider "aws" {
 resource "aws_s3_bucket" "this" {
   bucket        = var.bucket_name
   force_destroy = true
+  tags = {
+    "Environment" = "Dev"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "this" {
