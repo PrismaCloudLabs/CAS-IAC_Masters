0cb637926ffe34585233eafc0263c9c7286cfda9
Erick Moore <erick.moore@gmail.com>
2024-06-03 21:01:43 -0400

    adding tag

--- deploy/main.tf
+++ deploy/main.tf
@@ -6,7 +6,8 @@ resource "aws_s3_bucket" "this" {
   bucket        = var.bucket_name
   force_destroy = true
   tags = {
-    Environment = "Dev"
+    environnnment = "dev"
+    sensitivity = "secret"
   }
 }
 
